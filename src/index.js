4b7e893c44adb5f8c326b88f96f0bbdfa43889a6
Piftik <konstantin-poroh@mail.ru>
2023-01-10 23:32:55 +0300

    feat: answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const a = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty",
+    ];
+    const a20_100 = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const hundred = "hundred";
+    let x = number.toPrecision().split("");
+
+    if (number <= 20) {
+        return a[number];
+    }
+    if (number > 20 && number < 100) {
+        return +x[1] == 0
+            ? a20_100[x[0] - 2]
+            : a20_100[x[0] - 2] + " " + a[x[1]];
+    }
+    if (number >= 100) {
+        let y = a[x[0]] + " " + hundred;
+        let g = +(x[1] + x[2]) < 10 ? +x[2] : +(x[1] + x[2]);
+
+        if (+(x[1] + x[2]) < 21) {
+            return +(x[1] + x[2]) == 00 ? y : y + " " + a[g];
+        }
+        return +x[2] == 0
+            ? y + " " + a20_100[x[1] - 2]
+            : y + " " + a20_100[x[1] - 2] + " " + a[x[2]];
+    }
+};
